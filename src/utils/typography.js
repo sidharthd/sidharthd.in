78f4c86969896c7988918233e746177bbd761bd5
Sidharth D <sidharth@firstbytedigital.com>
2020-04-18 23:40:08 +0530

    Added typography.js

---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from 'typography';
+
+const typography = new Typography({
+  baseFontSize: '16px',
+  baseLineHeight: 1.666,
+  headerFontFamily: ['Montserrat', 'sans-serif'],
+  bodyFontFamily: ['Montserrat', 'sans-serif']
+});
+
+export default typography;
