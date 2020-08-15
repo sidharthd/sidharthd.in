48a14aad294c30a8f1c41bb887a143bcf4653816
Sidharth D <sidharth@firstbytedigital.com>
2020-08-15 23:55:18 +0530

    Added hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Avatar from '../components/avatar';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="hero-container">
+      <Avatar />
+      <h1 className="hero-text hero-designation">Co-Founder & CTO</h1>
+      <h1 className="hero-text">Firstbyte Digital Solutions</h1>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
